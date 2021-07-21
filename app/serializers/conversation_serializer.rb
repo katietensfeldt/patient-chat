929a66f7becc7b022b225cf2e791b754ee31aecc
Katie Tensfeldt <kmtensfeldt@gmail.com>
2021-07-21 14:21:58 -0700

    Updates conversation serializer to include last message

--- app/serializers/conversation_serializer.rb
+++ app/serializers/conversation_serializer.rb
@@ -1,7 +1,11 @@
 class ConversationSerializer < ActiveModel::Serializer
-  attributes :id, :patient_id, :partner_id
+  attributes :id, :patient_id, :partner_id, :last_message
 
   belongs_to :patient
   belongs_to :partner
   has_many :messages
+
+  def last_message
+    object.messages.last
+  end
 end
