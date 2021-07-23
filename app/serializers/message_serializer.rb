145a43ddadad0cc4b2d0577607d795a9da63f339
Katie Tensfeldt <kmtensfeldt@gmail.com>
2021-07-23 14:06:58 -0700

    Updates seeds file

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,5 +1,5 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :id, :body, :user_id
+  attributes :id, :body, :user_id, :created_at
   
   belongs_to :conversation
   belongs_to :user
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!([
+  {first_name: "Bob", last_name: "Smith", email: "bob@gmail.com", password: "password", type_of: "partner"},
+  {first_name: "Jane", last_name: "Doe", email: "jane@gmail.com", password: "password", type_of: "partner"},
+  {first_name: "Mike", last_name: "Person", email: "mike@gmail.com", password: "password", type_of: "partner"},
+  {first_name: "John", last_name: "Mann", email: "john@gmail.com", password: "password", type_of: "partner"},
+  {first_name: "Mary", last_name: "Lamb", email: "mary@gmail.com", password: "password", type_of: "partner"},
+  {first_name: "Katie", last_name: "User", email: "katie@gmail.com", password: "password", type_of: "patient"}
+])
