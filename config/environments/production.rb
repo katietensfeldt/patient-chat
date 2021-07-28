a47ed52c3977d5eff6b8c7c815ffa059d16f927f
Katie Tensfeldt <kmtensfeldt@gmail.com>
2021-07-28 10:26:05 -0700

    Fixes cable route

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   config.action_cable.mount_path = nil
-  config.action_cable.url = 'https://sheltered-dawn-14149.herokuapp.com/cable'
+  config.action_cable.url = 'wss://sheltered-dawn-14149.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [ 'https://patient-chat.netlify.app', /https:\/\/patient-chat.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
